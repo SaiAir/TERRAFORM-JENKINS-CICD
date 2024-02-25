285badf1f94aacf7aaf40be8664dc4b4c46e6a8b
SaiAir <157594591+SaiAir@users.noreply.github.com>
2024-02-26 02:49:50 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -15,6 +15,14 @@ resource "aws_s3_bucket_public_access_block" "example" {
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
+  bucket = aws_s3_bucket.mybucket.id
+  #acl    = "public-read"
+}
 resource "aws_s3_object" "index" {
   bucket = aws_s3_bucket.mybucket.id
   key = "index.html"
@@ -51,5 +59,7 @@ resource "aws_s3_bucket_website_configuration" "website" {
   error_document {
     key = "error.html"
   }
-  depends_on = [ aws_s3_bucket_acl.example ]
+  # Remove or comment out the next line since aws_s3_bucket_acl.example is no longer declared
+  # depends_on = [ aws_s3_bucket_acl.example ]
 }
+
