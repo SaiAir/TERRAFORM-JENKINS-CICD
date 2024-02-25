9c4bb41ed549040c5ad26076cd80bff99df7ff4c
SaiAir <157594591+SaiAir@users.noreply.github.com>
2024-02-26 02:45:18 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -15,14 +15,6 @@ resource "aws_s3_bucket_public_access_block" "example" {
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
-resource "aws_s3_bucket_acl" "example" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.example,
-    aws_s3_bucket_public_access_block.example,
-  ]
-  bucket = aws_s3_bucket.mybucket.id
-  #acl    = "public-read"
-}
 resource "aws_s3_object" "index" {
   bucket = aws_s3_bucket.mybucket.id
   key = "index.html"
