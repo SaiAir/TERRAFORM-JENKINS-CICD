785c4723de4e09449aec9fedfced8dee1147c6bf
SaiAir <157594591+SaiAir@users.noreply.github.com>
2024-02-26 01:17:50 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "sai-jenkins-cicd-777"
     key            = "my-terraform-environment/main"
-    region         = "ap-south-1"
-    dynamodb_table = "mrcloudbook-dynamo-db-table"
+    region         = "eu-west-1"
+    dynamodb_table = "sai-jenkins-cicd-dynamo-db"
   }
 }
